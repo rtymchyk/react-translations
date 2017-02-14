7a1c288073a85e8dc13d6f35e4e2547d459a61c8
Roman Tymchyk <roman@tilt.com>
2017-02-14 13:18:37 -0500

    New spec

--- test/LocalizedString_spec.js
+++ test/LocalizedString_spec.js
@@ -24,28 +24,28 @@ describe('LocalizedString', () => {
     Gettext._nc = sinon.spy(Gettext._nc);
   });
 
-  it('delegate singular to gettext client', () => {
+  it('invokes gettext as singular', () => {
     const output = render({ id: 'Hello' });
 
     expect(output.type).to.equal('span');
     expect(Gettext._.calledWith('Hello', 'en-US')).to.equal(true);
   });
 
-  it('delegate plural to gettext client', () => {
+  it('invokes gettext as plural', () => {
     const output = render({ id: 'One', idPlural: 'Many', count: 5 });
 
     expect(output.type).to.equal('span');
     expect(Gettext._n.calledWith('One', 'Many', 5, 'en-US')).to.equal(true);
   });
 
-  it('delegate singular with context to gettext client', () => {
+  it('invokes gettext as singular with context', () => {
     const output = render({ id: 'Hello', context: 'Context' });
 
     expect(output.type).to.equal('span');
     expect(Gettext._c.calledWith('Hello', 'Context')).to.equal(true);
   });
 
-  it('delegate plural with context to gettext client', () => {
+  it('invokes gettext as plural with context', () => {
     const output = render({
       id: 'One', idPlural: 'Many', count: 5, context: 'Context',
     });
